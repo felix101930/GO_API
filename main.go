84464019295e75f3182bda79f6d91beee8169030
felix-717 <vaghs622@gmail.com>
2025-02-24 12:57:36 -0700

    adds checkout path

--- main.go
+++ main.go
@@ -50,6 +50,32 @@ func getBooks(c *gin.Context) {
 
 }
 
+func checkoutBook(c *gin.Context) {
+
+	id,ok := c.GetQuery("id")
+	if !ok{
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Missing Book"})
+		return
+	}
+
+	book, err:= getBookById(id)
+
+	if err!=nil {
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Missing Book"})
+		return
+
+	}
+
+	if book.Quantity <= 0{
+		c.IndentedJSON(http.StatusBadRequest,gin.H{"message":"Book out of stock"})
+		return
+	}
+
+	book.Quantity =-1
+	c.IndentedJSON(http.StatusOK,book)
+
+}
+
 func bookById(c *gin.Context){
 	id := c.Param("id")
 	book, err := getBookById(id)
@@ -92,6 +118,7 @@ func main(){
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
 	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 
 
